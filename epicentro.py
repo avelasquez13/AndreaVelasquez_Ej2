d6ae9891f9a82fde6995463723b8ba68fd814754
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-04-21 16:29:17 -0500

    incluye condiciones iniciales

--- epicentro.py
+++ epicentro.py
@@ -1,2 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = np.array([2, -2, 5, 8, 5, 1])
+y = np.array([20, -1, 12, 10, -16, 40])
+z = np.array([0, 0, 0, 0, 0, 0])
+
+t = np.array([3.23, 3.82, 2.27, 3.04, 5.65, 6.57])
+
+sigma_t = 0.05 
+v = 5
