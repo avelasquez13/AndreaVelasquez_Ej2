18acb195126b42a7d8ef0abce9f05b09a30ddea7
fevemo <f.velasquez10@uniandes.edu.co>
2017-04-21 16:51:22 -0500

    Copia gelman rubin de Jaime

--- epicentro.py
+++ epicentro.py
@@ -13,11 +13,27 @@ v = 5
 def MCH:
     
 
-
-
-
-def RubinGel:
+def gelman_rubin(distro, obs_data, N=10000, M=4, sigma=0.1):
+    walks = {}
+    for m in range(M):
+        walks[m] = #Llamar a MCH sample_metropolis_hastings(distro, obs_data, N, sigma)
     
-
-
-
+    R = np.zeros(N-1)
+    for i in range(N-1):
+        n = i+1
+        mean_walks = np.zeros(M)
+        variance_walks = np.zeros(M)
+        for m in range(M):
+            mean_walks[m] = walks[m][:n].mean()
+            variance_walks[m] = walks[m][:n].std() ** 2
+        mean_general = mean_walks.mean()    
+        B = 0.0
+        for m in range(M):
+            B += (mean_walks[m] - mean_general)**2
+        B = n*B/(M-1)
+        W = variance_walks.mean()
+    
+        R[n-1] = (n-1)/n + (B/W)*(M+1)/(n*M)
+    
+    return walks, R
+    
